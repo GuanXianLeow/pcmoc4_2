6db9a978e04e18706bf97aab87c0f1d1069e89d9
Guan Xian <gxleow@gmail.com>
2020-12-17 10:46:12 +0800

    update

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -7,9 +7,24 @@ import {
   FlatList,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import firebase from "../database/firebaseDB";
 
 export default function NotesScreen({ navigation, route }) {
   const [notes, setNotes] = useState([]);
+
+  // When the screen loads, we start monitoring Firebase
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection("todos")
+      .onSnapshot((collection) => {
+        const updatedNotes = collection.docs.map((doc) => doc.data());
+        console.log(updatedNotes);
+        setNotes(updatedNotes);
+      });
+
+    return unsubscribe; // return the cleanup function
+  }, []);
 
   // This is to set up the top right button
   useEffect(() => {
@@ -38,7 +53,7 @@ export default function NotesScreen({ navigation, route }) {
         done: false,
         id: notes.length.toString(),
       };
-      setNotes([...notes, newNote]);
+      firebase.firestore().collection("todos").add(newNote);
     }
   }, [route.params?.text]);
 
